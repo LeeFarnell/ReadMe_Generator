bd5f35aa1bfed77a0df25c21defb3f1b3cfc1133
Lee Farnell <leefarnell@mac.com>
2021-05-03 13:46:13 +0100

    Initial question set up in questions and linked with init

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
 const questions = [
   // Define the questions.
+  {
+    type: "input",
+    message: "What is your full name?",
+    name: "fullName",
+  },
 ];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
 
 // TODO: Create a function to initialize app
-const init = () => {
+const init = async () => {
   // Ask the questions.
+  console.log("hello");
+
+  const answers = await inquirer.prompt(questions);
+
+  console.log(answers);
 };
 
 // Function call to initialize app
