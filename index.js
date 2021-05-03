677bc74d6137325279b2e487ead933153140d0e6
Lee Farnell <leefarnell@mac.com>
2021-05-03 13:23:25 +0100

    Added info to Sample Markdown and inserting string into generateMarkdown function

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // Define the questions.
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
 
 // TODO: Create a function to initialize app
-const init = () => {};
+const init = () => {
+  // Ask the questions.
+};
 
 // Function call to initialize app
 init();
